c6f265991e4596fc0f88793a2af36f6bed8d4ae7
Farou <fakhriamin0506@gmail.com>
2025-01-22 16:53:15 +0900

    Added some more hazards and added random angle spawn

--- Royal Run/Assets/05_Scripts/Levels/Hazard.cs
+++ Royal Run/Assets/05_Scripts/Levels/Hazard.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
-    [SerializeField] private GameObject hazardPrefab;
+    [SerializeField] private GameObject[] hazardPrefabs;
     [SerializeField] private float spawnYOffset;
     [SerializeField] private float[] lanes = { -2.8f, 0, 2.8f };
+    [SerializeField] private float hazardAnglesOffset = 10f;
 
     private void Start()
     {
@@ -17,6 +18,10 @@ public class Hazard : MonoBehaviour
     {
         float randomXPosition = lanes[Random.Range(0, lanes.Length)];
         Vector3 spawnPosition = new Vector3(randomXPosition, transform.position.y + spawnYOffset, transform.position.z);
-        Instantiate(hazardPrefab, spawnPosition, Quaternion.identity, transform);
+
+        GameObject randomHazard = hazardPrefabs[Random.Range(0, hazardPrefabs.Length)];
+
+        float randomAngle = Random.Range(-hazardAnglesOffset, hazardAnglesOffset);
+        Instantiate(randomHazard, spawnPosition, Quaternion.Euler(new Vector3(0, randomAngle, 0)), transform);
     }
 }
