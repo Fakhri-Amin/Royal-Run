1245ad0d8f0ec42d12cf2e32fe92c420f4abec51
Farou <86537710+Fakhri-Amin@users.noreply.github.com>
2025-02-09 22:23:45 +0900

    Added player hit animation

--- Royal Run/Assets/05_Scripts/Player/PlayerCollisionHandler.cs
+++ Royal Run/Assets/05_Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private float hitCooldown = 1f;
+    const string hitString = "Hit";
+
+    private float currentCooldown;
+
+    private void Start()
+    {
+        currentCooldown = hitCooldown;
+    }
+
+    private void Update()
+    {
+        currentCooldown -= Time.deltaTime;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
+        if (currentCooldown <= 0)
+        {
+            animator.SetTrigger(hitString);
+            currentCooldown = hitCooldown;
+        }
     }
 }
